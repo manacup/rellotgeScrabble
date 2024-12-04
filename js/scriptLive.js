16844dfe62f1ac64f08a7abc2868f3a641b311e2
manacup <91571517+manacup@users.noreply.github.com>
2024-12-04 18:01:30 +0100

    Update scriptLive.js

--- js/scriptLive.js
+++ js/scriptLive.js
@@ -24,10 +24,6 @@ var versio = document.querySelectorAll(".verdicc");
 
 //firebase
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
